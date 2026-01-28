72c4c07b58542105e6be9585faa443a412293cdc
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-28 14:35:45 -0500

    emp class

---
+++ ListExercise/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListExercise
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id}\nName = {Name}\nSalary = {Salary:C}";
+        }
+    }
+}
