0ea560b8de6c779155d065adcd4859229348ec53
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-28 13:35:37 -0500

    generic methods

--- W26Week4IntroToGenerics/Program.cs
+++ W26Week4IntroToGenerics/Program.cs
@@ -14,6 +14,25 @@
             // unboxing - ref type -> value type
             if (o is int)
                 i = (int)o;
+
+
+            //if (AreEqual(5, 5))
+            if (AreEqual<int>(5, 5))
+                Console.WriteLine("Both are equal");
+            else
+                Console.WriteLine("Both are not equal");
+        }
+
+        // standard, non-generic method
+        static bool AreEqual(object value1, object value2)
+        {
+            return value1.Equals(value2);
+        }
+
+        // generic method
+        static bool AreEqual<T>(T value1, T value2)
+        {
+            return value1.Equals(value2);
         }
     }
 }
