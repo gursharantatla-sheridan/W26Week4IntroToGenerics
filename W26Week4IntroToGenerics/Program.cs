0393032e32b096386a6a10e4314eb4d865a95220
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-28 13:18:57 -0500

    Add project files.

---
+++ W26Week4IntroToGenerics/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace W26Week4IntroToGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // boxing - value type -> ref type
+            int i = 5;
+            object o = i;
+
+            o = "hello";
+
+
+            // unboxing - ref type -> value type
+            if (o is int)
+                i = (int)o;
+        }
+    }
+}
