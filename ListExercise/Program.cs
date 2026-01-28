8a8181a16705fa24e7d03a43d60d4c83d8dd190d
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-28 14:44:38 -0500

    list eg

--- ListExercise/Program.cs
+++ ListExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee emp1 = new Employee(101, "John", 5000);
+            Employee emp2 = new Employee(102, "Anne", 7000);
+            Employee emp3 = new Employee(103, "Mark", 3000);
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(emp1);
+            employees.Add(emp2);
+            employees.Add(emp3);
+
+            employees.Insert(1, emp3);
+            employees.Remove(emp3);
+
+            foreach (Employee emp in employees)
+                Console.WriteLine(emp + "\n");
         }
     }
 }
