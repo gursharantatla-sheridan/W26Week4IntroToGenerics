7b4a5210e5a730e4a35c2f4829f1f98de7843e47
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-28 13:46:07 -0500

    array eg

---
+++ GenericCollections/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace GenericCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array
+            // GOOD - type safe
+            // BAD - static in size
+
+            int[] myArray = new int[3];
+            myArray[0] = 10;
+            myArray[1] = 20;
+            myArray[2] = 30;
+
+            //myArray[3] = "hello";
+            //myArray[3] = 40;
+
+            int sum = 0;
+
+            for (int i=0; i<myArray.Length; i++)
+            {
+                int num = myArray[i];
+                sum += num;
+            }
+
+            Console.WriteLine("Sum of Array items = " + sum);
+
+
+
+            // ArrayList
+
+        }
+    }
+}
